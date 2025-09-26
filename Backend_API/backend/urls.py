9bb96e704780e6fb1d106d20cd6efd295d67af36
Aditya Sharma <adishahil346@gmail.com>
2025-09-26 15:46:36 +0530

    Chat bot done

--- Backend_API/backend/urls.py
+++ Backend_API/backend/urls.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from accounts.views import  RegisterFaceView, ProfileViewSet, VerifyFaceView # <-- Import VerifyFaceView
-
+from chatbot.views import ChatbotView
 # Create a router and register our viewsets with it.
 router = DefaultRouter()
 router.register(r'profile', ProfileViewSet, basename='profile')
@@ -15,6 +15,7 @@ urlpatterns = [
     # Add the router's URLs to our urlpatterns.
     # This will create an endpoint at /api/profile/
     path('api/', include(router.urls)), 
+    path('api/chatbot/', ChatbotView.as_view(), name='chatbot'),
     
     # Existing auth and face registration URLs
     path('api/auth/', include('djoser.urls')),
